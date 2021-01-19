026c1ebb4c02d84f83983b32b5582ffb82985cd1
julogav <julietgavison@gmail.com>
2021-01-19 14:11:43 +0200

    working connection to mongodb and auth

--- auth.js
+++ auth.js
@@ -1,89 +1,95 @@
 const router = require('express').Router();
-const User = require('../model/User')
-const Pet = require('../model/petsSchema')
-const { registerValidation, loginValidation, petsValidation } = require("./validation");
-const bcrypt = require('bcryptjs')
+const User = require('./models/user');
+const { registerValidation, loginValidation } = require('./validation');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
 router.post('/register', async (req, res) => {
-    const { error } = registerValidation(req.body)
-    if (error) return res.status(400).send(error.details[0].message);
-    const emailExist = await User.findOne({ email: req.body.email })
-    if (emailExist) return res.status(400).send('Email already exists')
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-    const user = new User({
-        email: req.body.email,
-        password: hashPassword,
-        confirmPassword: hashPassword,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        phoneNumber: req.body.phoneNumber,
-    });
-    console.log(user);
-    try {
-        const saveUser = await user.save();
-        const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-        res.header('auth-token', token).send(token)
-    } catch (err) {
-        console.log(err);
-        res.status(400).send(err);
-    }
+	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+	const emailExist = await User.findOne({ email: req.body.email });
+	if (emailExist) return res.status(400).send('Email already exists');
+	const salt = await bcrypt.genSalt(10);
+	const hashPassword = await bcrypt.hash(req.body.password, salt);
+	const user = new User({
+		email: req.body.email,
+		password: hashPassword,
+		confirmPassword: hashPassword,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		phoneNumber: req.body.phoneNumber,
+	});
+	try {
+		const saveUser = await user.save();
+		const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+		res.header('auth-token', token).send(token);
+	} catch (err) {
+		console.log(err);
+		res.status(400).send(err);
+	}
 });
+
 router.post('/login', async (req, res) => {
-    const { error } = loginValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
-    const user = await User.findOne({ email: req.body.email })
-    if (!user) return res.status(400).send('Email is not found')
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if (!validPass) return res.status(400).send('Invalid password')
-    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-    res.setHeader("Access-Control-Allow-Headers", "token", token);
-    res.header('auth-token', token).send(token)
+	const { error } = loginValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) return res.status(400).send('Email is not found');
+	const validPass = await bcrypt.compare(req.body.password, user.password);
+	if (!validPass) return res.status(400).send('Invalid password');
+	const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+	res.setHeader('Access-Control-Allow-Headers', 'token', token);
+	res.header('auth-token', token).send(token);
 });
-router.put("/:id", async (req, res) => {
-    console.log(req.body);
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
-    const UpdatedUser = {
-        password: hashPassword,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        phoneNumber: req.body.phoneNumber,
-        email: req.body.email,
-    };
-    try {
-        console.log("email", req.body.email);
-        console.log('id', req.params.id);
-        const user = await User.findOneAndUpdate(
-            { _id: req.params.id },
-            { $set: UpdatedUser },
-            { new: true },
-            (err, UpdatedUser) => {
-                if (err) {
-                    return res.status(400).json(err)
-                }
-                res.json(UpdatedUser);
-            });
-    } catch (err) {
-        console.log(err);
-    }
+
+router.put('/:id', async (req, res) => {
+	console.log(req.body);
+	const salt = await bcrypt.genSalt(10);
+	const hashPassword = await bcrypt.hash(req.body.password, salt);
+	const UpdatedUser = {
+		password: hashPassword,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+		phoneNumber: req.body.phoneNumber,
+		email: req.body.email,
+	};
+	try {
+		console.log('email', req.body.email);
+		console.log('id', req.params.id);
+		const user = await User.findOneAndUpdate(
+			{ _id: req.params.id },
+			{ $set: UpdatedUser },
+			{ new: true },
+			(err, UpdatedUser) => {
+				if (err) {
+					return res.status(400).json(err);
+				}
+				res.json(UpdatedUser);
+			}
+		);
+	} catch (err) {
+		console.log(err);
+	}
 });
-router.get("/:id/full", async (req, res) => {
-    const UserID = req.params.id;
-    const user = await User.findOne({ _id: UserID });
-    res.send(user);
+
+router.get('/:id/full', async (req, res) => {
+	const UserID = req.params.id;
+	const user = await User.findOne({ _id: UserID });
+	res.send(user);
 });
-router.get("/", async (req, res) => {
-    const user = await User.find({});
-    res.send(user);
+
+router.get('/', async (req, res) => {
+	const user = await User.find({});
+	res.send(user);
 });
+
 router.get('/user', async (req, res) => {
-    try {
-        const user = await User.findOne({ _id: req.user.id });
-        res.json(user);
-    } catch (err) {
-        console.error(err.massage);
-        res.status(500).send('Server error');
-    }
+	try {
+		const user = await User.findOne({ _id: req.user.id });
+		res.json(user);
+	} catch (err) {
+		console.error(err.massage);
+		res.status(500).send('Server error');
+	}
 });
+
 module.exports = router;
--- index.js
+++ index.js
@@ -1,80 +1,35 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 const port = 5000;
 const cors = require('cors');
+// const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const Trip = require('./models/trip');
-const User = require('./models/user');
-
-app.use(express.json({ extended: false, limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-app.use(cors());
-dotenv.config();
+const authRoute = require('./auth');
 
 mongoose.connect(
 	'mongodb+srv://juliet:1312@cluster0.xwepi.mongodb.net/potatoes?retryWrites=true&w=majority',
 	{ useNewUrlParser: true, useUnifiedTopology: true },
 	() => console.log('connected to db')
 );
-let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-// app.use('/api/user', authRoute);
-// app.use('/api/pet', petRoute)
 
-app.post('/api/newtrip', (req, res) => {
+app.use(cors());
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+const db = mongoose.connection;
+
+app.use('/api/users', authRoute);
+
+app.post('/api/newtrip', (req, res, next) => {
 	const trip = new Trip(req.body);
 	trip.save();
 });
 
-app.post('/api/user/register', (req, res) => {
-	const user = new User(req.body);
-	console.log(req.body);
-	user.save();
-});
-
-// app.get('/api/tous-mes-pets', (req, res) => {
-// 	Pet.find({}, (err, doc) => {
-// 		res.json(doc);
-// 	});
-// });
-
-// app.get('/api/saved-pets', (req, res) => {
-// 	User.aggregate([
-// 		{
-// 			$match: {
-// 				email: 'julietgavison@gmail.com',
-// 			},
-// 		},
-// 		{
-// 			$lookup: {
-// 				from: 'pets',
-// 				localField: 'savedPets',
-// 				foreignField: '_id',
-// 				as: 'mypets',
-// 			},
-// 		},
-// 		{
-// 			$project: {
-// 				mypets: 1,
-// 				_id: 0,
-// 			},
-// 		},
-// 	])
-// 		.then(doc => res.json(doc))
-// 		.catch(err => console.error(err));
-// });
-
-// app.post('/api/log-in', (req, res) => {
-// 	User.findOne({ email: req.body.email });
-// });
-
-// app.get('/api/all-users', (req, res) => {
-// 	User.find({}, (err, doc) => {
-// 		res.json(doc);
-// 	});
-// });
-
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+	console.log(`app listening at http://localhost:${port}`);
 });
