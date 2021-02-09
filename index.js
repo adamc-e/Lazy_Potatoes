bbb1884a5f657f722ca26663f02fb57ada34f70b
naor798 <naorkahalany@gmail.com>
2021-02-09 23:06:36 +0200

    it's workingggg

--- index.js
+++ index.js
@@ -1,27 +1,57 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 5000;
-const cors = require('cors');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Trip = require('./models/trip');
 const authRoute = require('./routes/auth');
-// const tripsRoute = require('./routes/tripsRoute');
+// const tripsRoute = require('./routes/tripsRoute')
+const cors = require('cors');
 
-mongoose.connect(
-	'mongodb+srv://juliet:1312@cluster0.xwepi.mongodb.net/potatoes?retryWrites=true&w=majority',
-	{ useNewUrlParser: true, useUnifiedTopology: true },
-	() => console.log('connected to db')
-);
+const connectDB = async () => {
+	try {
+		await mongoose.connect(process.env.mongoURI, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+			useUnifiedTopology: true,
+		});
+		console.log('MongoDB Connected');
+	} catch (err) {
+		console.error(err);
+		process.exit(1);
+	}
+};
+connectDB()
+
+// mongoose.connect(
+// 	'mongodb+srv://juliet:1312@cluster0.xwepi.mongodb.net/potatoes?retryWrites=true&w=majority',
+// 	{ useNewUrlParser: true, useUnifiedTopology: true },
+// 	() => console.log('connected to db')
+// );
 
 app.use(cors());
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
 app.use('/api/users', authRoute);
 // app.use('/api/trips', tripsRoute);
+
+app.get('/api/alltrips', async (req, res) => {
+	try {
+		const trips = await Trip.find({})
+		res.json(trips)
+
+	} catch (err) {
+		console.error(err)
+
+	}
+	// Trip.find({}, (err, doc) => {
+	// 	res.json(doc);
+	// });
+});
 
 app.post('/api/newtrip', (req, res, next) => {
 	try {
@@ -33,16 +63,12 @@ app.post('/api/newtrip', (req, res, next) => {
 	}
 });
 
-app.put('/api/addRequest', async (req, res) => {
-	const id = req.body.tripId;
-	const request = req.body;
-});
+// app.put('/api/addRequest', async (req, res) => {
+// 	const id = req.body.tripId;
+// 	const request = req.body;
+// });
 
-app.get('/api/alltrips', (req, res) => {
-	Trip.find({}, (err, doc) => {
-		res.json(doc);
-	});
-});
+
 
 app.get('/api/alltrips/search', async (req, res) => {
 	try {
